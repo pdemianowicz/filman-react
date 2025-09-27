d13af156e4d97dc14de9c2053098652f6c9a5540
pdemianowicz <pdemianowicz@gmail.com>
2025-09-27 18:46:04 +0200

    build: Initialize project with Vite, React, TypeScript & Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-text-primary">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <p className="mt-4 font-medium text-lg text-text-secondary">Welcome to my react app!</p>
+    </div>
+  );
+}
+
+export default App;
